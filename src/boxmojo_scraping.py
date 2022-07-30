f431dceb6d6146a31baddcc011a33507798faa53
db495 <78694520+db495@users.noreply.github.com>
2022-07-30 16:28:29 +0100

    refactoring code

--- src/boxmojo_scraping.py
+++ src/boxmojo_scraping.py
@@ -4,13 +4,9 @@
 import requests
 from bs4 import BeautifulSoup as BS
 import requests
-import sqlite3
 from datetime import datetime
-from dateutil.parser import parse
 import re
 
-conn = sqlite3.connect('movie_opening_weekend.db')
-cursor = conn.cursor()
 
 #url request. Must insert url with quotations "url"
 def request_func (url):
@@ -33,15 +29,9 @@ def movie_titles (soup):
 # This function scrapes the opening weekend gross revenue
 def opening_weekend_gross (soup):
     gross_opening=soup.find_all('td', class_='a-text-right mojo-field-type-money')
-    i = 0
-    opening_weekend = []
-    for record in gross_opening:
-        if i % 2 != 0:
-            opening_weekend.append(record.string)
-        else 
-          continue
-        i += 1
-        
+    
+    opening_weekend = [record.string for record in gross_opening]
+  
     opening_figures = []
     for record in opening_weekend:
         try:
@@ -52,8 +42,8 @@ def opening_weekend_gross (soup):
             
     all_opening_figures = []
     for record in opening_figures:
-        if type(item) == int:
-            all_opening_figures.append(item)
+        if type(record) == int:
+            all_opening_figures.append(record)
         else:
             all_opening_figures.append('NAN')
             continue
@@ -67,55 +57,47 @@ def release_and_closing_dates (soup):
     i = 0
     for record in opening_and_closing_date:
         if i % 2 == 0:
-          try:
-             opening_dates.append(record.string)
-          except:
-              opening_dates.append('NAN')
-             continue
+            try:
+                opening_dates.append(record.string)
+            except:
+                opening_dates.append('NAN')
         else:
-          try:
-             closing_dates.append(record.string)
-          except:
-              closing_dates.append('NAN')
-             continue
+            try:
+                closing_dates.append(record.string)
+            except:
+                closing_dates.append('NAN')
         i += 1
             
-    opening_dates_clean = []
-    closing_dates_clean = []
-    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    month_dict = {month:f"{datetime.datetime.strptime(month, '%b').month:02}" for month in months}
+  #  opening_dates_clean = []
+ #   closing_dates_clean = []
+ #   months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+ #   month_dict = {month:f"{datetime.strptime(month, '%b').month:02}" for month in months}
    
-    for key, value in month_dict.items():
-       opening_dates_clean = [re.sub(key, value, date) for date in opening_dates]
-    print(opening_dates_clean)
+ #    for key, value in month_dict.items():
+#        opening_dates_clean = [re.sub(key, value, date) for date in opening_dates]
+#    print(opening_dates_clean)
     
 # WIP to get the cleaned versions of dates
             
-    return opening_dates_clean, closing_dates_clean
+    return opening_dates, closing_dates
 
 # This function creates a tuple to combine all the different pieces of data extracted together, so it can be inserted into a database and written out to a dataframe. 
-def create_tuples(movie_titles, all_opening_figures):
+def create_tuples(movie_titles, all_opening_figures, release_and_closing_dates, year):
     combined_dataset = []
+    i=0
     for record in range(len(movie_titles)):
-        new_tuple = (movie_titles[i],all_opening_figures[i], opening_dates_clean, closing_dates_clean)
+        new_tuple = (movie_titles[i],all_opening_figures[i], release_and_closing_dates[0][i], release_and_closing_dates[1][i], year)
         combined_dataset.append(new_tuple)
+        i += 1
     return combined_dataset
-  
-# This function adds the tuples to the created database
-def add_to_database (conn, cursor, combined_dataset):
-    statement = """INSERT INTO mojo_data (title, opening_earnings, opening_date, closing_date) 
-    VALUES (%s, %s, %s, %s)"""
-    cursor.executemany(statement, combined_dataset)
-    conn.commit()
 
 # This is the main function which joins all the previous supporting functions together and runs them in sequence to create a full database with all the relevant records in it
-def extract_opening_weekend_data (url):
+def extract_opening_weekend_data (url, year):
     soup = request_func(url)
-    movie_titles = movie_titles(soup)
-    opening_numbers_with_none = opening_weekend_gross(soup)
+    movie_title = movie_titles(soup)
+    all_opening_figures = opening_weekend_gross(soup)
     opening_closing_dates = release_and_closing_dates(soup)
-    final_dataset = create_tuples (movie_titles, all_opening_figures)
-    add_to_database(conn, cursor, final_dataset)
+    final_dataset = create_tuples (movie_title, all_opening_figures, opening_closing_dates, year)
     return final_dataset
 
     
